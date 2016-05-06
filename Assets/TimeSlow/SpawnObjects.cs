140b33df804f9a464ba735ddb83773469834c1f7
Marian Busila <marian@imposition.com>
2016-05-06 19:21:38 -0400

    TimeSlow

---
+++ Assets/TimeSlow/SpawnObjects.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnObjects : MonoBehaviour {
+
+    public GameObject objectToSpawn;
+    public float delay = 1f;
+	// Use this for initialization
+	void Start () {
+        Invoke("Spawn", delay);
+	}
+
+	void Spawn () {
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+        Invoke("Spawn", delay);
+    }
+}
---
+++ Assets/TimeSlow/TimeSlow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeSlow : MonoBehaviour {
+
+    public static TimeSlow Instance;
+    public float defaultTimeScale = 0.5f;
+    public float defaultDuration = 5f;
+    public float defaultTransitionTime = 1f;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+	}
+
+    public void SlowTime()
+    {
+        SlowTime(defaultTimeScale, defaultDuration, defaultTransitionTime);
+    }
+	
+	public void SlowTime(float timeScale, float duration, float transitionTime)
+    {
+        StopAllCoroutines();
+        StartCoroutine(ActuallyTimeSlow(timeScale, duration, transitionTime));
+    }
+
+    IEnumerator ActuallyTimeSlow(float timeScale, float duration, float transitionTime)
+    {
+        float initialTime = Time.realtimeSinceStartup;
+
+        //Transition in
+        while(Time.realtimeSinceStartup - initialTime < transitionTime)
+        {
+            Time.timeScale = Mathf.Lerp(1.0f, timeScale, (Time.realtimeSinceStartup - initialTime) / transitionTime);
+            yield return null;
+        }
+
+        initialTime = Time.realtimeSinceStartup;
+        //Duration
+        while (Time.realtimeSinceStartup - initialTime < duration)
+        {
+            yield return null;
+        }
+
+        initialTime = Time.realtimeSinceStartup;
+        //Transition out
+        while (Time.realtimeSinceStartup - initialTime < transitionTime)
+        {
+            Time.timeScale = Mathf.Lerp(timeScale, 1.0f, (Time.realtimeSinceStartup - initialTime) / transitionTime);
+            yield return null;
+        }
+    }
+}
+
