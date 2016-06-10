cd7d9b1604fc6167cd13ac8ef47a37b9a2c810c3
Marian Busila <marian@imposition.com>
2016-06-10 17:41:35 -0400

    CameraSwitch

---
+++ Assets/CameraSwitch/SwitchCamera.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchCamera : MonoBehaviour {
+
+    public Camera newCamera;
+    private bool activeNow = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	void Interact ()
+    {
+        activeNow = !activeNow;
+
+        if (activeNow)
+        {
+            newCamera.enabled = true;
+            //GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = false;
+        }
+        else
+        {
+            newCamera.enabled = false;
+            //GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>().enabled = true;
+        }
+	}
+}
