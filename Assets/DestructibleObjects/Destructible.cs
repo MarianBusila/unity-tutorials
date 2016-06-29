71704490a86c1c1f917ff76e4bbd2478bd2c91af
Marian Busila <marian@imposition.com>
2016-06-29 00:31:20 -0400

    DestructibleObjects using Blender CellFracture

---
+++ Assets/DestructibleObjects/Destructible.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destructible : MonoBehaviour {
+
+    public GameObject debrisPrefab;
+	void OnMouseDown()
+    {
+        DestroyMe();
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.impactForceSum.magnitude > 1.0f)
+            DestroyMe();
+    }
+
+    void DestroyMe()
+    {
+        if (debrisPrefab)
+        {
+            Instantiate(debrisPrefab, transform.position, transform.rotation);
+        }
+        Destroy(gameObject);
+    }
+}
