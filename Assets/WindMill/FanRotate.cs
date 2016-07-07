c6038e8ae97da7952a22c7b2a393a9f20f31c817
Marian Busila <marian@imposition.com>
2016-07-06 23:14:53 -0400

    Windmill

---
+++ Assets/WindMill/FanRotate.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FanRotate : MonoBehaviour {
+
+    public float RotationSpeed;
+    void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0f, RotationSpeed, 0f));
+    }
+}
