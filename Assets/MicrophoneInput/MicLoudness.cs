529041812db085f188b7bc9288b236e799dd39d9
Marian Busila <marian.busila@gmail.com>
2016-09-28 01:16:51 -0400

    MicrophoneInput

---
+++ Assets/MicrophoneInput/MicLoudness.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MicLoudness : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float loudness = MicInput.loudness;
+        Debug.Log("loudness: " + loudness);
+        if(loudness > 10.0f)
+        {
+            Vector3 previousScale = transform.localScale;
+            previousScale.y = loudness / 10f;
+            transform.localScale = previousScale;
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+        }
+    }
+}
