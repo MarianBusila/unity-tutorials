5f3da3d3c30a0fbfd085482f4ebab49c79d65b46
Marian Busila <marian.busila@gmail.com>
2016-10-22 21:16:05 -0400

    MoveOnRail

---
+++ Assets/MoveOnRail/Mover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+    public Rail rail;
+    public PlayMode playMode;
+
+    private int currentSeg = 0;
+    private float transition;
+    private bool isCompleted;
+
+    private void Update()
+    {
+        if (!rail)
+            return;
+                
+        if (!isCompleted)
+        {
+            Debug.Log("Segment: " + currentSeg);
+            Play();
+        }
+    }
+
+    private void Play()
+    {
+        transition += Time.deltaTime * 1 / 2.5f;
+        if(transition > 1)
+        {
+            transition = 0;
+            currentSeg++;
+        }
+
+        if (currentSeg + 1 == rail.Length)
+            isCompleted = true;
+        else
+        {
+            transform.position = rail.PositionOnRail(currentSeg, transition, playMode);
+            transform.rotation = rail.Orientation(currentSeg, transition);
+        }
+    }
+}
