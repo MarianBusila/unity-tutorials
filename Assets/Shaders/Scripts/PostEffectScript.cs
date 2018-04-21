9a1be7f335c1be0ace87086d561a84a90f4f7e57
Marian Busila <marian.busila@gmail.com>
2018-04-21 11:49:06 -0400

    Shaders - Added a PostEffectMaterial which uses a custom invert color shader. Created script which applies the material in OnRenderImage. Attached script to main camera

---
+++ Assets/Shaders/Scripts/PostEffectScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffectScript : MonoBehaviour {
+    public Material postEffectMaterial;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        // apply changes to source before passing it on
+        Graphics.Blit(source, destination, postEffectMaterial);
+    }
+}
